6c144c8bf43336b27ca707a4b3b5867a0039c1d2
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-06-27 19:47:45 +0200

    Add stop play pause

--- js/app.js
+++ js/app.js
@@ -1,33 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const chronoDisplay = document.getElementById("chrono")
-    const startBtn = document.getElementById("start")
-    const pauseBtn = document.getElementById("pause")
-    const stopBtn = document.getElementById("stop")
+    let startBtn = document.getElementById("start")
+    let pauseBtn = document.getElementById("pause")
+    let stopBtn = document.getElementById("stop")
 
+    let start
+    let sec = 0
+    let min = 0
+    let hour = 0
+    let tic = 0
 
-let sec = 0
-let min = 0
-let hour = 0
-let tic = 0
+    let chrono = () => {
+        tic++
+        sec = tic
+        chronoDisplay.innerHTML = `${hour} : ${min} : ${sec}`
+        if (tic > 59) {
+            min++
+            tic = 0
+            if (min > 59) {
+                hour++
+                min = 0
+                if (hour > 24) {
+                    hour = 0
+                }
+            }
+        }
+    }
 
-//Fonction qui ajoute des secondes
-let chrono = () =>{
-tic ++
-    //A 60 sec faire 1min ...
-if(tic>59){
-    min ++
-    tic = 0
-}
-sec = tic
-chronoDisplay.innerHTML = `${hour} : ${min} : ${sec}`
-}
-
-startBtn.addEventListener('click', () => {
-    setInterval(chrono,1000)
-})
-//Stoper le chronometre
-
-//Mettre en pause le chronomètre
-
+    //Todo faire possibilité de cliquer une seul fois
+    startBtn.addEventListener('click', (event) => {
+        start = setInterval(chrono, 1000)
+        if (event.detail > 1){
+            clearInterval(start)
+        }
+    })
+    //Stoper le chronometre
+    stopBtn.addEventListener('click', () => {
+        clearInterval(start)
+        tic = 0
+        sec = 0
+        hour = 0
+        sec = 0
+        chronoDisplay.innerHTML = `00:00:00`
+    })
+    //Mettre en pause le chronomètre
+    pauseBtn.addEventListener('click', () => {
+        clearInterval(start)
+    })
 })
