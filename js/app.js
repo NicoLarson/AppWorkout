ff578dee5cb32f726c80b06b0cec1d4e179a7feb
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-06-27 08:55:11 +0200

    Add chrono

--- js/app.js
+++ js/app.js
@@ -4,15 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.getElementById("start")
     const pauseBtn = document.getElementById("pause")
     const stopBtn = document.getElementById("stop")
-    let tic = 0
-    let chrono = chronoDisplay.innerHTML
-
-    startBtn.addEventListener('click', () => {
-        setInterval(chrono, 1000)
-        chronoDisplay.innerHTML = tic + 1
-        console.log(tic)
-    })
 
 
+let sec = 0
+let min = 0
+let hour = 0
+let tic = 0
+
+//Fonction qui ajoute des secondes
+let chrono = () =>{
+tic ++
+    //A 60 sec faire 1min ...
+if(tic>59){
+    min ++
+    tic = 0
+}
+sec = tic
+chronoDisplay.innerHTML = `${hour} : ${min} : ${sec}`
+}
+
+startBtn.addEventListener('click', () => {
+    setInterval(chrono,1000)
+})
+//Stoper le chronometre
+
+//Mettre en pause le chronomètre
 
 })
