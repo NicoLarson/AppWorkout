775cbe0348b37574d31b83dee68c191372ba9d8d
Yang <nicolas.yang.1@gmail.com>
2020-10-11 11:39:33 +0200

    modif css

---
+++ js/appHiit.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+//* Paramètres
+    // nombres de Tours
+    let round
+    // Période de travail
+    let timerWork
+    // Période de repos
+    let timerRes
+    
+    
+    
+    // Chronomètre pour afficher le temps total
+    let totalTime
+
+
+
+    /* 
+    Cliquer sur Démarrer
+    Compte à rebours 3 secondes
+    Déclenchement du chronomètre + timer
+    Tant que le nombre de tours n'est pas terminer
+        Temps de travail s'écoule
+            Fin du temps de travail
+        Temps de repos se déclenche
+            Fin du temps de repos
+        Ajouter un tour
+    
+        *Possibilité de passer le temps de travail ou le temps de repos
+    
+    */
+})
